f9a536c6c4073623072219fc2fb84f9a6d76acf4
edu-rodrigues5 <126525398+edu-rodrigues5@users.noreply.github.com>
2024-09-09 22:36:08 -0300

    Update LivroDAL.java

--- LivroDAL.java
+++ LivroDAL.java
@@ -18,6 +18,7 @@ public class LivroDAL {
     private static ObjectContainer db;
     //static ObjectSet<Livro> lista;
     static int i=4;
+    static int a=0;
 
 public static void conecta()
     {
@@ -34,23 +35,32 @@ public static Livro getLivro(int op)
 {
     conecta();
     ObjectSet<Livro> lista = db.get(Livro.class);
-    System.out.println("Na lista temos " + lista.size() + " livros.");
-    
+    Livro livro = null;
     switch(op)
     {
         case 2:
             i=0;break;
-        case 3:int a=i;
-            if(a++ >= 0){i=i-1;}break;
-        case 4: int b=i;
-            if(b++<= 6){i=i+1;}break;
+        case 3: a=i;
+            if(a-- >= 0){i=i-1;}else{i=0;}
+                break;
+        case 4: a=i;
+            if(a++<= 6){i=i+1;}else{i=6;}
+                break;
         case 5:
             i=6;break;
     }
-    Livro livro = lista.get(i);
+    //ARRUMAR
+    if(i >= 0 && i <= 6)
+    {
+        livro = lista.get(i);
+        desconecta();
+        return livro;
+       
+    }
+    else{
+        desconecta();
+        return null;
+    }
     
-    desconecta();
-    
-    return livro;
 }
 }
